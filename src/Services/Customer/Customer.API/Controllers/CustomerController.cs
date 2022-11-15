d140d37e8cccd7b91a8d917d226540d673d3d2e3
emre keles <emre.keles@tomdigital.com.tr>
2022-11-15 23:40:51 +0300

    Docker compose files added Part-1

--- src/Services/Customer/Customer.API/Controllers/CustomerController.cs
+++ src/Services/Customer/Customer.API/Controllers/CustomerController.cs
@@ -15,6 +15,7 @@ namespace Customer.API.Controllers
     {
         private readonly ICustomerRepository _repository;
         private readonly IPublishEndpoint _publishEndpoint;
+        // private readonly ISendEndpointProvider _sendEndpointProvider;
 
         public CustomerController(IPublishEndpoint publishEndpoint, ICustomerRepository repository)
         {
@@ -36,7 +37,8 @@ namespace Customer.API.Controllers
         {
             await _repository.CreateCustomer(customerEntity);
             var eventMessage = new CustomerCreationEvent { Email = customerEntity.Email, Name = customerEntity.Name, Surname = customerEntity.Surname };
-
+            //var sendEndPoint = await _sendEndpointProvider.GetSendEndpoint(new Uri("queue:create-order-service")); //MassTransit sending...
+            //await sendEndPoint.Send<CreateOrderMessageCommand>(createOrderMessageCommand); //sending a message to RMQ as a command type.
             await _publishEndpoint.Publish<CustomerCreationEvent>(eventMessage); // Publish to MessageBroker
 
             return Ok(customerEntity);
--- src/WebApps/HealthWebStatus/Controllers/HomeController.cs
+++ src/WebApps/HealthWebStatus/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using HealthWebStatus.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace HealthWebStatus.Controllers
 {
