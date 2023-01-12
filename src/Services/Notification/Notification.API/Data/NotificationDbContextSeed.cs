2ac9c0a8eb7cd5ccab1a52542288c8c1132b572c
emre keles <emre.keles@tomdigital.com.tr>
2023-01-12 22:44:48 +0300

    Database migration processes

---
+++ src/Services/Notification/Notification.API/Data/NotificationDbContextSeed.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Notification.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Notification.API.Data
+{
+    public class NotificationDbContextSeed
+    {
+        public static async Task SeedAsync(NotificationDbContext dbContext, ILogger<NotificationDbContextSeed> logger)
+        {
+            if (!dbContext.Notifications.Any()) // check any product in the db
+            {
+                dbContext.Notifications.AddRange(InsertDummyNotifications());
+                await dbContext.SaveChangesAsync();
+                logger.LogInformation("------Seed database associated with context {DbContextName}------", typeof(NotificationDbContext).Name);
+            }
+        }
+
+
+        private static IEnumerable<NotificationCustomer> InsertDummyNotifications()
+        {
+            return new List<NotificationCustomer>
+            {
+                new NotificationCustomer(){  Name = "Ali", Surname = "Alioglu", Email = "developer.emrekeles@gmail.com"},
+            };
+        }
+    }
+}
--- src/Services/Notification/Notification.API/Program.cs
+++ src/Services/Notification/Notification.API/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Notification.API.Data;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Notification.API.Infrastructure.Extentions;
 
 namespace Notification.API
 {
@@ -7,7 +11,13 @@ namespace Notification.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build()
+              .MigrateDatabase<NotificationDbContext>((context, services) => 
+               {
+                  var logger = services.GetService<ILogger<NotificationDbContextSeed>>();
+                  NotificationDbContextSeed.SeedAsync(context, logger).Wait(); // after Migration, seed the Db
+               })
+              .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
